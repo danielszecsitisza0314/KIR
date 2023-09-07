233665be347929e165cec6fef6e97d95e822dc1e
Dani <daniel.szecsitisza@gmail.com>
2023-09-07 18:36:55 +0200

    full

--- src/core/Database.php
+++ src/core/Database.php
@@ -4,4 +4,6 @@ function getConnection()
 {
     return new pdo('mysql:host=' . $_SERVER['DB_HOST'] . ';dbname=' . $_SERVER['DB_NAME'], $_SERVER['DB_USER'], $_SERVER['DB_PASSWORD']);
 } // létre jön a kapcsolat az adatbázissal egy .htacces fileon keresztül
+
+
 ?>
--- src/index.php
+++ src/index.php
@@ -1,19 +1,19 @@
 <?php
 include_once __DIR__ . '/core/Database.php';
-header('Content-Type: text/html; charset=iso-8859-2');
 $method = $_SERVER["REQUEST_METHOD"];
 $parsed = parse_url($_SERVER['REQUEST_URI']);
 $path = $parsed['path'];
 
+//print_r(getPathWhitId(($_SERVER['HTTP_REFERER'])));
 $routes = [
     'GET' => [
-        "/" => 'menu',
-        "/mainmenu" => 'menu'
+        "/" => 'home',
     ],
     'POST' => [
-        "/mainmenu" => 'loginhandler',
+        "/login" => 'loginhandler',
         "/logout" => 'logoutHandler',
-        "/changepassword" => 'changepassword'
+        "/changepassword" => 'changepassword',
+        "/newuseradd" => 'register'
     ]
 ];
 session_start();
@@ -22,38 +22,32 @@ $handlerFunction = $routes[$method][$path] ?? "notFoundHandler";
 
 $handlerFunction();
 
-function loginForm()
-{
-    echo compileTemplate('wrapper.phtml', [
-        'content' => compileTemplate('login.phtml', []),
-        'bejelentkezve' => false
-    ]);
-}
 
-function menu()
+function home()
 {
     if (isLoggedIn()) {
         echo compileTemplate('wrapper.phtml', [
             'content' => compileTemplate('mainMenu.phtml', []),
-            'bejelentkezve' => true
+            'bejelentkezve' => isLoggedIn()
         ]);
     } else {
-        loginForm();
+        echo compileTemplate('wrapper.phtml', [
+            'content' => compileTemplate('login.phtml', []),
+            'bejelentkezve' => isLoggedIn()
+        ]);
+        session_unset();
     }
-}
-
-function compileTemplate($filePath, $params = []): string
-{
-    ob_start();
-    require __DIR__ . "/views/" . $filePath;
-    return ob_get_clean();
 }
 
 function notFoundHandler()
 {
-    echo "Oldal nem található";
+    echo compileTemplate("wrapper.phtml", [
+        'content' => compileTemplate('notfoundPage.phtml'),
+        'bejelentkezve' => isLoggedIn()
+    ]);
 }
 
+// ha hibás az url akkor erre az oldalra dob át
 function isLoggedIn(): bool
 {
     if (! isset($_COOKIE[session_name()])) {
@@ -63,13 +57,13 @@ function isLoggedIn(): bool
     if (! isset($_SESSION['userID'])) {
         return false;
     }
-
     return true;
 }
 
 // vizsgálja hogy a böngésző tárol e sütit
 function loginhandler()
 {
+    $err = 'success';
     $pdo = getConnection();
     $statement = $pdo->prepare("SELECT * FROM users WHERE user_name = ? ");
     $statement->execute([
@@ -78,48 +72,109 @@ function loginhandler()
     $user = $statement->fetch(PDO::FETCH_ASSOC);
 
     if (! $user) {
-        header('Location: ' . 'mainmenu' . '&info=invalidDate');
+        $err = 'invalidData';
+        $_SESSION['checkPassword'] = $err;
+        $_SESSION['username'] = $_POST["username"];
+        header('Location: ' . '/');
         return;
     }
-    if (! (make($_POST['pw']) === $user["password"])) {
-        header('Location: ' . 'mainmenu' . '&info=invalidDate');
+    if (! (make($_POST['pw']) === $user["password"]) && $user['active'] === 'Igen') {
+        $err = 'invalidData';
+        $_SESSION['checkPassword'] = $err;
+        $_SESSION['username'] = $_POST["username"];
+        header('Location: ' . '/');
         return;
     }
-    $_SESSION['userID'] = $user['id'];
-    $_SESSION['username'] = $user['user_name'];
-    $_SESSION['fullname'] = $user['full_name'];
-    $_SESSION['pw'] = $user['password'];
-    $_SESSION['email'] = $user['email'];
-    $_SESSION['governmentoffice'] = $user['government_office'];
-    $_SESSION['accounttype'] = $user['account_type'];
-    $_SESSION['firstlogin'] = $user['first_login'];
-    $_SESSION['NFA'] = $user['NFA'];
-    $_SESSION['GINOP'] = $user['GINOP'];
-    $_SESSION['TOP'] = $user['TOP'];
-    $_SESSION['active'] = $user['active'];
-    if ($_SESSION['active'] == 'Igen') {
-        menu();
+
+    if ($user['active'] === 'Igen') {
+        $_SESSION['checkPassword'] = $err;
+        header('Location: ' . '/');
+        $_SESSION['userID'] = $user['id'];
+        $_SESSION['username'] = $user['user_name'];
+        $_SESSION['fullname'] = $user['full_name'];
+        $_SESSION['pw'] = $user['password'];
+        $_SESSION['email'] = $user['email'];
+        $_SESSION['governmentoffice'] = $user['government_office'];
+        $_SESSION['accounttype'] = $user['account_type'];
+        $_SESSION['firstlogin'] = $user['first_login'];
+        $_SESSION['NFA'] = $user['NFA'];
+        $_SESSION['GINOP'] = $user['GINOP'];
+        $_SESSION['TOP'] = $user['TOP'];
+        $_SESSION['active'] = $user['active'];
+        return;
     } else {
-        header('Location: ' . 'mainmenu' . '&info=accountIsNotActive');
+        $err = 'accountIsNotActive';
+        $_SESSION['username'] = $_POST["username"];
+        $_SESSION['checkPassword'] = $err;
+        header('Location: ' . '/');
+        return;
     }
 }
 
 function changepassword()
 {
     if (! (make($_POST['curr_pass']) === $_SESSION['pw'])) {
-        header('Location: ' . 'changepassword' . '&info=wrongPassword');
+        $passwordCheck = make($_POST['curr_pass']);
+        echo $passwordCheck;
         return;
     }
-    // update data -> itt kell jav�tani
+    
     $pdo = getConnection();
     $statement = $pdo->prepare("UPDATE users SET password = ? WHERE user_name = ?");
     $statement->execute([
         make($_POST['password']),
         $_SESSION['username']
     ]);
+    unset($_SESSION['pw']);
     $_SESSION['pw'] = make($_POST['password']);
+    $passwordCheck = 'ok';
+    echo $passwordCheck;
+    return;
+}
+
+function register()
+{
+    $pdo = getConnection();
+    $statement = $pdo->prepare("SELECT * FROM users WHERE user_name = ? ");
+    $statement->execute([
+        $_POST["userName"]
+    ]);
+    $user = $statement->fetch(PDO::FETCH_ASSOC);
+    if ($user) {
+        header('Location: ' . getPathWhitId($_SERVER['HTTP_REFERER']) . '&info=regisztracioSikertelen');
+        return;
+    } elseif ($_POST["user_name"] === "" || $_POST["full_name"] === "" || $_POST["email"] === "" || $_POST["government_office"] === "" || $_POST["account_type"] === "" || $_POST["NFA"] === "" || $_POST["GINOP"] === "" || $_POST["TOP"] === "") {
+        header('Location: ' . getPathWhitId($_SERVER['HTTP_REFERER']) . '&info=regisztracioSikertelen');
+        return;
+    } else {
+        $statement = $pdo->prepare("INSERT INTO users (user_name, full_name, email, password, government_office, account_type, active, NFA, GINOP, TOP, first_login) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $statement->execute([
+            $_POST['userName'],
+            $_POST['fullName'],
+            $_POST['email'],
+            make('abc123'),
+            $_POST['list_jaras'],
+            $_POST['list_accounttype'],
+            'Igen',
+            $_POST['NFA'],
+            $_POST['GINOP'],
+            $_POST['TOP'],
+            'Igen'
+        ]);
+    }
     header('Location: ' . '/');
 }
+
+function logoutHandler()
+{
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', 0, $params['path'], $params['domain'], $params['secure'], isset($params['httponly']));
+    // session_unset();
+    session_destroy();
+    header('Location: ' . '/');
+}
+
+// kijelentkezés gombra kattintva eldobja a sütiket és nem lesznek elérhetőek azok az oldalak amik bejelentkezve elérhetőek voltak
 
 // ezt még ki kell tapasztalni!
 function getPathWhitId($url)
@@ -130,23 +185,19 @@ function getPathWhitId($url)
     }
     $queryParams = [];
     parse_str($parsed['query'], $queryParams);
-    return $parsed['path'] . "?id=" . $queryParams['id'];
+    return $parsed['path'] . $queryParams['id'];
 }
 
-function logoutHandler()
-{
-    $params = session_get_cookie_params();
-
-    setcookie(session_name(), '', 0, $params['path'], $params['domain'], $params['secure'], isset($params['httponly']));
-    // session_unset();
-    session_destroy();
-    header('Location: ' . '/');
-}
-
-// kijelentkezés gombra kattintva eldobja a sütiket és nem lesznek elérhetőek azok az oldalak amik bejelentkezve elérhetőek voltak
 function make($string, $salt = '')
 {
     return hash('sha256', $string . $salt);
+}
+
+function compileTemplate($filePath, $params = []): string
+{
+    ob_start();
+    require __DIR__ . "/views/" . $filePath;
+    return ob_get_clean();
 }
 
 ?>
